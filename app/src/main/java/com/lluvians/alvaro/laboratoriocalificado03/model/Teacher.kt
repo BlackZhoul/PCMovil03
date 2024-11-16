5353c1d774b6c409330e065b7bb2dde716d10e20
BlackZhoul <alvaro.lluviians@gmail.com>
2024-11-16 12:41:55 -0500

    first commit

---
+++ app/src/main/java/com/lluvians/alvaro/laboratoriocalificado03/model/Teacher.kt
@@ -0,0 +1,9 @@
+package com.lluvians.alvaro.laboratoriocalificado03.model
+
+data class Teacher(
+    val name: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String,
+    val imageUrl: String
+)
